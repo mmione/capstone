2f3931b0903e49c8afdb8bda47ef2e9707036411
mmione <mattmione37@gmail.com>
2021-01-07 17:00:28 -0500

    Updated to show more relevant plots

--- physics_test.m
+++ physics_test.m
@@ -1,14 +1,17 @@
 % Initializing arrays for calculations % 
 
-angles = [10:1:40];
+angles = [10:1:40]';
 masses = [];
 torques = [];
 
-for massSystem = [0.2704:0.1:1] % Loop from minimum (w/o battery) to 1kg 
+massBattery = 0.6332739; % Change these as needed
+massNoBattery = 0.2864; 
+
+for massSystem = [[massNoBattery:0.1:massBattery] massBattery] % Loop from minimum (w/o battery) to 1kg 
     
     % Givens %
 
-    diskMass = 0.2;
+    diskMass = 0.6;
     radiusDisk = 0.07;
     RPM = 400;
     %massSystem = 0.2704;
@@ -31,8 +34,9 @@ end
 
 % Plotting 
 
-plot(angles, torques);
+plot(angles, torques(1:31), angles, torques(1+1*31:31+1*31), angles, torques(1+2*31:31+2*31), angles, torques(1+3*31:31+3*31), angles, torques(1+4*31:31+4*31));
 title("Max Angle vs Required Torque")
 xlabel("Angle [deg]");
 ylabel("Torque [N*m]");
-
+% legend("Without battery","","","With Battery (approx)");
+yline(0.0392266,'--',"Motor Threshold");
