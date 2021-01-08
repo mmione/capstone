ad2cf235790546c7da2d74f49e7b19185beb7c31
mmione <mattmione37@gmail.com>
2021-01-07 22:53:42 -0500

    Fixed legend, cleaned up code

--- physics_test.m
+++ physics_test.m
@@ -1,17 +1,17 @@
 % Initializing arrays for calculations % 
 
-angles = [10:1:40]';
+angles = [10:1:40]'; % Use to test all angles within a reasonable range
 masses = [];
 torques = [];
 
 massBattery = 0.6332739; % Change these as needed
-massNoBattery = 0.2864; 
+massNoBattery = 0.2064; 
 
 for massSystem = [[massNoBattery:0.1:massBattery] massBattery] % Loop from minimum (w/o battery) to 1kg 
     
     % Givens %
 
-    diskMass = 0.6;
+    diskMass = 0.2;
     radiusDisk = 0.07;
     RPM = 400;
     %massSystem = 0.2704;
@@ -29,14 +29,15 @@ for massSystem = [[massNoBattery:0.1:massBattery] massBattery] % Loop from minim
     
     torques = [torques torque]; % Changes size dynamically as the loop iterates. Bad practice?
     masses(end+1) = massSystem;
-    %test
+    
 end
 
 % Plotting 
 
 plot(angles, torques(1:31), angles, torques(1+1*31:31+1*31), angles, torques(1+2*31:31+2*31), angles, torques(1+3*31:31+3*31), angles, torques(1+4*31:31+4*31));
 title("Max Angle vs Required Torque")
+yline(0.0392266,'--',"Motor Threshold");
 xlabel("Angle [deg]");
 ylabel("Torque [N*m]");
-% legend("Without battery","","","With Battery (approx)");
-yline(0.0392266,'--',"Motor Threshold");
+legend("Without battery m = "+string(masses(1)),"m = "+string(masses(2)),"m = "+string(masses(3)), "m = "+string(masses(3)), "m = "+string(masses(4)), "With Battery (approx) m = "+string(masses(5)));
+
