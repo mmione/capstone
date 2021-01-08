1d55b8ebac7bf4e9775b4d0f53bf356397347b8b
mmione <mattmione37@gmail.com>
2021-01-07 23:21:12 -0500

    Cleanup code and add README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+#physical-simulations
+
+* MATLAB script to help us simulate different scenarios for our capstone project
+* Tests for how required performance changes versus different variables
--- physics_test.m
+++ physics_test.m
@@ -36,7 +36,7 @@ end
 
 plot(angles, torques(1:31), angles, torques(1+1*31:31+1*31), angles, torques(1+2*31:31+2*31), angles, torques(1+3*31:31+3*31), angles, torques(1+4*31:31+4*31));
 title("Max Angle vs Required Torque")
-yline(0.0392266,'--',"Motor Threshold");
+yline(0.0392266,'--',"Motor Threshold"); % Creating line for motor threshold
 xlabel("Angle [deg]");
 ylabel("Torque [N*m]");
 legend("Without battery m = "+string(masses(1)),"m = "+string(masses(2)),"m = "+string(masses(3)), "m = "+string(masses(3)), "m = "+string(masses(4)), "With Battery (approx) m = "+string(masses(5)));
