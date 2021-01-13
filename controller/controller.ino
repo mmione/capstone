7ba0ffed3c9d2290e5a07107d2f44142f62a03aa
mmione <mattmione37@gmail.com>
2021-01-13 13:20:29 -0500

    Adding controller sketch, capable of reading RPM from motor

---
+++ controller/controller.ino
@@ -0,0 +1,56 @@
+#define ENCODER_INPUT_1 3
+#define ENCODER_INPUT_2 4
+
+#define GEAR_RATIO 20
+#define ENCODER_FACTOR 12
+
+// Need to use volatile variables as their values may be messed with by the interrupts
+
+volatile bool motordir = false; 
+volatile uint32_t lastA = 0; // Unsigned int, 32bit long
+volatile float RPM = 0; 
+
+void isr(){
+
+  // Read motor's direction
+  motordir = digitalRead(ENCODER_INPUT_2);
+
+  uint32_t currA = micros(); // Take snapshot of time
+
+  if(lastA < currA){
+
+    float rev = currA - lastA;  // us
+    rev = 1.0 / rev;            // rev per us
+    rev *= 1000000;             // rev per sec
+    rev *= 60;                  // rev per min
+    rev /= GEAR_RATIO;             // account for gear ratio
+    rev /= ENCODER_FACTOR;         // account for multiple ticks per rotation
+    RPM = rev;
+    
+  }
+
+  lastA = currA;
+}
+
+
+void setup() {
+
+  // Setting up pins
+  pinMode(ENCODER_INPUT_1, INPUT_PULLUP);
+  pinMode(ENCODER_INPUT_2, INPUT_PULLUP);
+
+  // One of the pins needs to be an interrupt pin, for the rotary encoder idea to work.
+  attachInterrupt(digitalPinToInterrupt(ENCODER_INPUT_1), isr, RISING);
+
+  // Serial for debugging 
+  Serial.begin(9600);
+  
+}
+
+
+void loop() {
+
+  Serial.println((int)RPM);
+  delay(5);
+  
+}
